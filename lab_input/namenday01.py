53245028322ee4fc93588ad3665a0d334e910200
cboz23 <chris.bozley@gmail.com>
2024-01-02 22:13:21 +0000

    learning about input function

---
+++ lab_input/namenday01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+
+    user_name = input("Hi, enter your name: ")
+    dayoftheweek = input("Also, enter the day of the week: ")
+
+    print(f'Hello, {user_name}! Happy {dayoftheweek}!')
+
+main()
