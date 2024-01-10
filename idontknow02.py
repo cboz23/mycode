511cc341ad1761d510ee8377160735701e4cbdaf
cboz23 <chris.bozley@gmail.com>
2024-01-10 17:55:48 +0000

    Learning about sub classes

--- idontknow02.py
+++ idontknow02.py
@@ -1,81 +1,91 @@
-import tkinter as tk
-from tkinter import messagebox
 import random
+import time
+import sys
 
-class DateNightApp:
-    def __init__(self, master):
-        self.master = master
-        master.title("Date Night Food Suggester")
+def loading_spinner():
+    spinner = "|/-\\"
+    for _ in range(10):
+        for char in spinner:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(0.1)
+            sys.stdout.write("\b")
 
-        self.food_choices = [
-            "Italian (Pasta, Pizza)",
-            "Japanese (Sushi, Ramen, Hibachi)",
-            "Mexican (Tacos, Burritos)",
-            "Mediterranean (Hummus, Kebabs, Shawarmas)",
-            "Thai (Pad Thai, Curry)",
-            "Indian (Curry, Biriyani, Tandoori)",
-            "Steakhouse",
-            "Vietnamese (Pho, Bahn Mi, Com Ga)",
-            "Seafood (Crab, Shrimp, Lobster, Fish)",
-            "Pizza",
-            "Chicken (Wings, Fried Chicken, Chicken Sandwiches)",
-            "Vegetarian/Vegan",
-            "Food from your favorite local restaurant",
-        ]
+def suggest_food():
+    # Food options for date night
+    food_choices = [
+        "Italian (Pasta, Pizza)",
+        "Japanese (Sushi, Ramen, Hibachi)",
+        "Mexican (Tacos, Burritos)",
+        "Mediterranean (Hummus, Kebabs, Shawarmas)",
+        "Thai (Pad Thai, Curry)",
+        "Indian (Curry, Biriyani, Tandoori)",
+        "Steakhouse",
+        "Vietnamese (Pho, Bahn Mi, Com Ga)",
+        "Seafood (Crab, Shrimp, Lobster, Fish)",
+        "Pizza",
+        "Chicken (Wings, Fried Chicken, Chicken Sandwiches)",
+        "Vegetarian/Vegan",
+        "Food from your favorite local restaurant",
+    ]
 
-        self.create_widgets()
+    # Ask the user to select five choices
+    print("Choose five food options for your date night (enter numbers 1-{}):".format(len(food_choices)))
+    for i, choice in enumerate(food_choices, start=1):
+        print(f"{i}. {choice}")
 
-    def create_widgets(self):
-        self.label = tk.Label(self.master, text="Choose five food options for your date night (enter numbers 1-13):")
-        self.label.pack()
+    user_selection = set()
+    while len(user_selection) < 5:
+        try:
+            choice_number = int(input("Enter the number of your choice: "))
+            if 1 <= choice_number <= len(food_choices):
+                user_selection.add(food_choices[choice_number - 1])
+            else:
+                print("Invalid choice. Please enter a number between 1 and {}.".format(len(food_choices)))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
-        self.choices_var = tk.StringVar(value="")
-        for i, choice in enumerate(self.food_choices, start=1):
-            tk.Radiobutton(self.master, text=choice, variable=self.choices_var, value=choice).pack()
+    if len(user_selection) != 5:
+        print("You must select exactly five options. Exiting.")
+        return
 
-        self.submit_button = tk.Button(self.master, text="Submit", command=self.submit_choices)
-        self.submit_button.pack()
+    # Simulate loading spinner during randomization
+    print("\nRandomizing choices: ", end="")
+    loading_spinner()
 
-    def submit_choices(self):
-        user_selection = self.choices_var.get().split()
-        if len(user_selection) != 5:
-            messagebox.showinfo("Error", "You must select exactly five options.")
-            return
+    # Randomly select three choices from the user's selection
+    random_choices = random.sample(list(user_selection), k=3)
 
-        random_choices = random.sample(user_selection, k=3)
+    # Ask the user to choose two from the three random choices
+    print("\n\nYou selected these five options:")
+    for i, choice in enumerate(food_choices, start=1):
+        if choice in user_selection:
+            print(f"{i}. {choice}")
 
-        user_final_choices = self.select_final_choices(random_choices)
-        if user_final_choices is not None:
-            final_choice = user_final_choices.pop()
-            messagebox.showinfo("Result", f"Dinner has been decided! Enjoy some {final_choice} cuisine!")
-        else:
-            messagebox.showinfo("Error", "You must select exactly two options.")
+    print("\nNow, choose two from the following three options:")
+    for i, choice in enumerate(random_choices, start=1):
+        print(f"{i}. {choice}")
 
-    def select_final_choices(self, random_choices):
-        final_choices_var = tk.StringVar(value="")
-        for i, choice in enumerate(random_choices, start=1):
-            tk.Radiobutton(self.master, text=choice, variable=final_choices_var, value=choice).pack()
+    user_final_choices = set()
+    while len(user_final_choices) < 2:
+        try:
+            choice_number = int(input("Enter the number of your final choices: "))
+            if 1 <= choice_number <= 3:
+                user_final_choices.add(random_choices[choice_number - 1])
+            else:
+                print("Invalid choice. Please enter a number between 1 and 3.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
-        submit_button = tk.Button(self.master, text="Submit", command=lambda: self.display_final_choice(final_choices_var))
-        submit_button.pack()
+     # Simulate loading spinner during randomization
+    print("\nRandomizing choices: ", end="")
+    loading_spinner()
 
-        self.submit_button.config(state="disabled")
-        self.label.config(text="Now, choose two from the following three options:")
-
-        self.master.mainloop()
-        return final_choices_var.get().split()
-
-    def display_final_choice(self, final_choices_var):
-        self.master.quit()
-        final_choice = final_choices_var.get().split()
-        if len(final_choice) == 2:
-            messagebox.showinfo("Result", f"Dinner has been decided! Enjoy some {final_choice[0]} and {final_choice[1]} cuisine!")
-        else:
-            messagebox.showinfo("Error", "You must select exactly two options.")
-        self.master.destroy()
+    if len(user_final_choices) == 2:
+        final_choice = user_final_choices.pop()
+        print(f"\nDinner has been decided! Enjoy some {final_choice} cuisine! If you're disappointed in the selection, then it seems you know what you really want!")
+    else:
+        print("You must select exactly two options. Exiting.")
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = DateNightApp(root)
-    root.mainloop()
-
+    suggest_food()
